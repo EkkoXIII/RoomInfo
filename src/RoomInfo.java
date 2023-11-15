a5a7aae1acc47df923d21fe810c2e9e62f51fb56
EkkoXIII <astaroth644@gmail.com>
2023-11-15 19:00:26 +0300

    Внес правки согласно комментариям наставника

--- src/RoomInfo.java
+++ src/RoomInfo.java
@@ -16,13 +16,17 @@ public class RoomInfo {
 
         //Перед тем, как искать информацию по квартире, проверяем, что она есть в доме
         //Если нет - выводим сообщение и завершаем выполнение
+
         int roomsPerFloor = 4;
+
         if ((apartNumber < 1) || (apartNumber > (roomsPerFloor * floorsQuantity * entrancesQuantity))) {
             System.out.println("Такой квартиры в доме нет");
             return;
         }
+
         //Если квартира в доме есть, то продолжаем выполнение
         //Находим расположение квартиры
+
         String apartLoc = null;
         switch (apartNumber % roomsPerFloor) {
             case 0 -> apartLoc = "Справа от лифта, вправо";
@@ -30,21 +34,29 @@ public class RoomInfo {
             case 2 -> apartLoc = "Слева от лифта, вправо";
             case 3 -> apartLoc = "Справа от лифта, влево";
         }
+
         // Находим номер подъезда
+
         int entranceNum;
+
         if (apartNumber % (roomsPerFloor * floorsQuantity) == 0) {
             entranceNum = apartNumber / (roomsPerFloor * floorsQuantity);
         } else {
             entranceNum = apartNumber / (roomsPerFloor * floorsQuantity) + 1;
         }
+
         //Находим номер этажа
+
         int floorNum;
+
         if (apartNumber % roomsPerFloor == 0) {
             floorNum = apartNumber / roomsPerFloor - (entranceNum - 1) * floorsQuantity;
         } else {
             floorNum = apartNumber / roomsPerFloor - (entranceNum - 1) * floorsQuantity + 1;
         }
+
         //Выводим результат
+
         System.out.println(apartNumber + " кв - " + entranceNum + " подъезд, " + floorNum + " этаж, " + apartLoc);
     }
 }
